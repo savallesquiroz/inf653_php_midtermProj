5603270dc7b748f103672611ac60abb7dd2bdfcc
Steve Valles <savallesquiroz@mail.fhsu.edu>
2025-03-10 02:17:07 +0100

    Refactor Author API: Improve delete and update functionality with better validation and error handling

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -1,17 +1,25 @@
 <?php
+require_once '../../config/Database.php';
+require_once '../../models/Author.php';
+
+$database = new Database();
+$db = $database->connect();
+
+// Get JSON input
 $data = json_decode(file_get_contents("php://input"));
 
-if (!isset($data->id)) {
-    echo json_encode(['message' => 'Missing Required Parameters']);
+// Validate that id is provided
+if(empty($data->id)){
+    echo json_encode(array("message" => "Missing Required Parameter: id"));
     exit();
 }
 
-$query = "DELETE FROM authors WHERE id = :id";
-$stmt = $author->conn->prepare($query);
+$author = new Author($db);
+$author->id = $data->id;
 
-if ($stmt->execute([':id' => $data->id])) {
-    echo json_encode(['message' => 'Author Deleted']);
+if($author->delete()){
+    echo json_encode(array("message" => "Author Deleted"));
 } else {
-    echo json_encode(['message' => 'Author Not Deleted']);
+    echo json_encode(array("message" => "Author Not Deleted"));
 }
 ?>
--- api/authors/update.php
+++ api/authors/update.php
@@ -1,32 +1,26 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: PUT');
-header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-
 require_once '../../config/Database.php';
 require_once '../../models/Author.php';
 
 $database = new Database();
 $db = $database->connect();
-$author = new Author($db);
 
+// Get JSON input
 $data = json_decode(file_get_contents("php://input"));
 
-if (!isset($data->id, $data->author)) {
-    echo json_encode(['message' => 'Missing Required Parameters']);
+// Validate required fields (both id and author are needed)
+if(empty($data->id) || empty($data->author)){
+    echo json_encode(array("message" => "Missing Required Parameters"));
     exit();
 }
 
-$query = "UPDATE authors SET author = :author WHERE id = :id";
-$stmt = $db->prepare($query);
+$author = new Author($db);
+$author->id = $data->id;
+$author->author = $data->author;
 
-if ($stmt->execute([
-    ':id' => $data->id,
-    ':author' => $data->author
-])) {
-    echo json_encode(['message' => 'Author Updated']);
+if($author->update()){
+    echo json_encode(array("message" => "Author Updated"));
 } else {
-    echo json_encode(['message' => 'Author Not Updated']);
+    echo json_encode(array("message" => "Author Not Updated"));
 }
 ?>
